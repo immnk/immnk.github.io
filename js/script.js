3c426a5c1a34afb0d2ee24a54ce8f5e3ccb05729
Manikanta <im.manikanta@gmail.com>
2016-01-07 15:02:20 -0500

    Basic template

---
+++ js/script.js
@@ -0,0 +1,8 @@
+angular.module('starter.controllers', []);
+angular.module('starter.services', []);
+
+angular.module('amrutha', ['ui.bootstrap', 'ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+});
