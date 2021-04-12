6104cade0ad6a6f48bb9d056f9073135adea748f
Mani <im.manikanta@gmail.com>
2021-04-12 22:09:27 +0800

    wip

---
+++ src/App/App.js
@@ -0,0 +1,54 @@
+import css from "./App.css";
+
+function App() {
+  console.log(css);
+  return (
+    <div className={css.root}>
+      <div className={css.devImage}></div>
+      <div className={css.container}>
+        <h1 className={css.header}>
+          Hi, I'm <span className={css.highlight}>Mani 👓</span>
+        </h1>
+        <p>
+          Mobile and Front End Developer | UI UX | Web Designer | Foodie | Tech
+          Enthusiastic | Developer #DBS | Proud Indian
+        </p>
+        <div className={css.iconsSocial}>
+          <a
+            target="_blank"
+            href="https://github.com/immnk"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a
+            target="_blank"
+            href="https://twitter.com/manimnk"
+            rel="noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            target="_blank"
+            href="https://stackoverflow.com/story/manikantamnk"
+            rel="noreferrer"
+          >
+            <i className="fab fa-stack-overflow"></i>
+          </a>
+          <a target="_blank" href="https://dev.to/immnk" rel="noreferrer">
+            <i className="fab fa-dev"></i>
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/manikantatankala"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
